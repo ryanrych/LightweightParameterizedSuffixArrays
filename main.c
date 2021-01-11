05b9f2dc416c29f0f5b44e3fcca821a9da12dcb1
ryanrych <ryanjrychlak@gmail.com>
2021-01-11 03:02:10 -0600

    Added the pv and fw encoding algorithms.

--- main.c
+++ main.c
@@ -1,6 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node{
+    char key;
+    int val;
+    struct node *next;
+};
+struct table{
+    int size;
+    struct node **list;
+};
+struct table *createTable(int size){
+    struct table *t = (struct table*)malloc(sizeof(struct table));
+    t->size = size;
+    t->list = (struct node**)malloc(sizeof(struct node*)*size);
+    int i;
+    for(i=0;i<size;i++)
+        t->list[i] = NULL;
+    return t;
+}
+int hashCode(struct table *t,char key){
+    return ((int) key)%t->size;
+}
+void insert(struct table *t,char key,int val){
+    int pos = hashCode(t,key);
+    struct node *list = t->list[pos];
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+    struct node *temp = list;
+    while(temp){
+        if(temp->key==key){
+            temp->val = val;
+            return;
+        }
+        temp = temp->next;
+    }
+    newNode->key = key;
+    newNode->val = val;
+    newNode->next = list;
+    t->list[pos] = newNode;
+}
+int lookup(struct table *t,char key){
+    int pos = hashCode(t,key);
+    struct node *list = t->list[pos];
+    struct node *temp = list;
+    while(temp){
+        if(temp->key==key){
+            return temp->val;
+        }
+        temp = temp->next;
+    }
+    return -1;
+}
+
+int* pv(char* str, int pi, int n){
+    struct table* map = createTable(pi);
+    int* encoded = (int*) malloc(n * sizeof(int));
+    for (int i=0; i<n; i++){
+        int lastOcc = lookup(map, str[i]);
+        if (lastOcc == -1){
+            encoded[i] = 0;
+            insert(map, str[i], i);
+        }else{
+            encoded[i] = i - lastOcc;
+            insert(map, str[i], i);
+        }
+    }
+    return encoded;
+}
+
+int* fw(char* str, int pi, int n){
+    struct table* map = createTable(pi);
+    int* encoded = (int*) malloc(n * sizeof(int));
+    for (int i=n-1; i>=0; i--){
+        int lastOcc = lookup(map, str[i]);
+        if (lastOcc == -1){
+            encoded[i] = n;
+            insert(map, str[i], i);
+        }else{
+            encoded[i] = lastOcc - i;
+            insert(map, str[i], i);
+        }
+    }
+    return encoded;
+}
+
 int partition(int left, int right, int pivot, int* A){
     int partitionIndex = left;
     for (int i=left; i<=right; i++){
@@ -35,7 +118,6 @@ void quicksort(int left, int right, int* A) {
 }
 
 int main() {
-
 
     return 0;
 }
